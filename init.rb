7bb57b789bb8c58f4ef13124b1d1511bc8e34b9e
haru_iida <devnull@localhost>
2011-12-11 17:42:33 +0900

    fixes #898 compatible with Redmine 1.3.0

--- init.rb
+++ init.rb
@@ -34,8 +34,8 @@ Redmine::Plugin.register :redmine_theme_changer do
   name 'Redmine Theme Changer plugin'
   author 'Haruyuki Iida'
   description 'Lets each user select a theme for Redmine'
-  version '0.0.4'
+  version '0.0.5'
   url 'http://www.r-labs.org/projects/themechanger'
   author_url 'http://twitter.com/haru_iida'
-  requires_redmine :version_or_higher => '1.1.0'
+  requires_redmine :version_or_higher => '1.3.0'
 end
