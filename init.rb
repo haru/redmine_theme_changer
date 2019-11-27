1ae6d41b4ff61f321176e7f0c5fd6836366b7ca2
Lxc <mel+lxc@ennder.fr>
2019-11-27 14:49:04 +0100

    require -> require_dependency for debug environment
    require_dependency 'theme_changer_my_account_hooks'
    require_dependency 'theme_changer_user_patch'
    require_dependency 'theme_changer_themes_patch'
    modifié :         init.rb

--- init.rb
+++ init.rb
@@ -16,11 +16,12 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'redmine'
-require 'theme_changer_my_account_hooks'
-require 'theme_changer_user_patch'
-require 'theme_changer_themes_patch'
 
 Rails.configuration.to_prepare do
+  require_dependency 'theme_changer_my_account_hooks'
+  require_dependency 'theme_changer_user_patch'
+  require_dependency 'theme_changer_themes_patch'
+
   # Guards against including the module multiple time (like in tests)
   # and registering multiple callbacks
   require_dependency 'user_preference'
