288f01674664f0ffe6ae7c3144ae1c7fe2fa45f7
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-12-23 12:57:15 +0000

    fix: update test helper and application helper tests for theme changer functionality

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,33 +1,30 @@
-require 'simplecov'
-require 'simplecov-rcov'
-require 'simplecov-lcov'
-# require 'coveralls'
+require "rails"
+require "simplecov"
+require "simplecov-lcov"
 
 SimpleCov::Formatter::LcovFormatter.config do |config|
   config.report_with_single_file = true
-  config.single_report_path = File.expand_path(File.dirname(__FILE__) + '/../coverage/lcov.info')
+  config.single_report_path = File.expand_path(File.dirname(__FILE__) + "/../coverage/lcov.info")
 end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::RcovFormatter,
   SimpleCov::Formatter::LcovFormatter,
   SimpleCov::Formatter::HTMLFormatter
-  # Coveralls::SimpleCov::Formatter
 ]
 
 SimpleCov.start do
-  root File.expand_path(File.dirname(__FILE__) + '/..')
+  root File.expand_path(File.dirname(__FILE__) + "/..")
   add_filter "/test/"
 end
 
-require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+# Load the normal Rails helper
+require File.expand_path(File.dirname(__FILE__) + "/../../../test/test_helper")
 
-# Configure fixture path for plugin
-# Rails 6.1 uses fixture_path (singular), Rails 7+ uses fixture_paths (plural)
-if ActiveSupport::TestCase.respond_to?(:fixture_paths)
-  # Rails 7+
-  ActiveSupport::TestCase.fixture_paths << File.expand_path('../fixtures', __FILE__)
-elsif ActiveSupport::TestCase.respond_to?(:fixture_path)
-  # Rails 6.1
-  ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
+fixtures = []
+Dir.chdir(File.dirname(__FILE__) + "/fixtures/") do
+  fixtures = Dir.glob("*.yml").map { |s| s.gsub(/.yml$/, "") }
 end
+ActiveRecord::FixtureSet.create_fixtures(File.dirname(__FILE__) + "/fixtures/", fixtures)
+
+# Ensure that we are using the temporary fixture path
+#Engines::Testing.set_fixture_path
