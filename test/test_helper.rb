1f5b02a25dfd26a57161e97af19aae544934bcb1
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-12-23 12:39:11 +0000

    fix: update fixture path configuration for compatibility with Rails 6.1 and 7+

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,4 +23,11 @@ end
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
 # Configure fixture path for plugin
-ActiveSupport::TestCase.fixture_paths << File.expand_path('../fixtures', __FILE__)
+# Rails 6.1 uses fixture_path (singular), Rails 7+ uses fixture_paths (plural)
+if ActiveSupport::TestCase.respond_to?(:fixture_paths)
+  # Rails 7+
+  ActiveSupport::TestCase.fixture_paths << File.expand_path('../fixtures', __FILE__)
+elsif ActiveSupport::TestCase.respond_to?(:fixture_path)
+  # Rails 6.1
+  ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
+end
